d907ef40fd65c17bb37adb7ccf2a5049b4adc66f
Sahan <sahangeesaradev11@proton.me>
2025-02-22 18:17:15 +0530

    create add new page functionality

--- routes/cms_handler.py
+++ routes/cms_handler.py
@@ -20,6 +20,15 @@ class CMSHandler(BaseHandler):
         elif parsed_url.path == "/cms/cms_templates/css/styles.css":
             self.path = "cms_templates/css/styles.css"
             return
+        elif parsed_url.path == "/cms/edit/new_page":
+            name, avatar_url = self._get_authenticated_user()
+            if name:
+                server.serve_html.serve_html_template(self, "cms_templates/edit/new_page.html", {
+                    "name": name,
+                    "avatar_url": avatar_url,
+                    "page_content": ""
+                })
+            return
         edit_match = re.match(r"^/cms/edit/([\w-]+)\.html$", parsed_url.path)
         if edit_match:
             page_name = edit_match.group(1)
@@ -56,6 +65,36 @@ class CMSHandler(BaseHandler):
     def do_POST(self):
         parsed_url = urlparse(self.path)
 
+        if parsed_url.path == "/cms/edit/new_page":
+            content_length = int(self.headers.get("Content-Length", 0))
+            post_data = self.rfile.read(content_length)
+            form = parse_qs(post_data.decode("utf-8"))
+
+            if "page_name" in form:
+                raw_page_name = form["page_name"][0].strip()
+                page_name = re.sub(r"[^\w-]", "", raw_page_name)
+
+                if not page_name:
+                    self.send_error(400, "Invalid page name")
+                    return
+
+                page_path = f"pages/{page_name}.html"
+
+                if not os.path.exists(page_path):
+                    os.makedirs(os.path.dirname(page_path), exist_ok=True)
+
+                    default_content = "<html><head><title>New page</title></head><body><h1>Welcome!</h1></body></html>"
+
+                    with open(page_path, "w", encoding="utf-8", newline="\n") as file:
+                        file.write(default_content)
+
+                    self.redirect(f"/cms/edit/{page_name}.html")
+                    return
+                else:
+                    self.send_error(409, "Page already exists")
+            else:
+                self.send_error(400, "Missing 'page_number' in request")
+            return
         edit_match = re.match(r"^/cms/edit/([\w-]+)\.html", parsed_url.path)
         if edit_match:
             # get post data html_code san write it to the file
@@ -79,7 +118,6 @@ class CMSHandler(BaseHandler):
                             file.write(decoded_html)
 
                         self.redirect(f"/cms/edit/{page_name}.html")
-
                         return
                     else:
                         self.send_error(400, "Invalid request: Missing 'html_code'")
